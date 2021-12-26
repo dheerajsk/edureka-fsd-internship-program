49f4300ae1d298588600ec85949f4e9c45552d33
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-12-26 11:00:18 +0530

    Math functions

---
+++ Module3 - JS/maths.js
@@ -0,0 +1,30 @@
+
+const x=4.9;
+const y=4.2;
+const z = 4.5;
+
+// round
+console.log(Math.round(x));
+console.log(Math.round(y));
+console.log(Math.round(z));
+
+// ceil (Rounds up)
+console.log("Ceil====");
+console.log(Math.ceil(x));
+console.log(Math.ceil(y));
+console.log(Math.ceil(z));
+
+// floor (Rounds down)
+console.log("Floor====");
+console.log(Math.floor(x));
+console.log(Math.floor(y));
+console.log(Math.floor(z));
+
+const value = -102.34;
+// Absolute positive value
+console.log(Math.abs(value));
+
+const val = 16;
+console.log(Math.sqrt(val));
+
+console.log(Math.pow(4, 4));
